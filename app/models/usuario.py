fff96b080b43a724df148e68dce8eb893f2b032a
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-11-10 15:27:12 +0000

    update

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -12,10 +12,64 @@ class Usuario(Base):
     email = Column(String(150), unique=True) # Não aceita e-mail repetido.
     senha = Column(String(150))
 
-    def __init__(self, nome: str, email: str, senha: str):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
+def __init__(self, nome: str, email: str, senha: str):
+    self.nome = self._verificar_nome_usuario(nome)
+    self.email = self._verificar_email_usuario(email)
+    self.senha = self._verificar_senha_usuario(senha)
+
+def _verificar_nome_usuario(self, nome):
+
+    self._verificar_nome_invalido(nome)
+    self._verificar_nome_vazio(nome)
+
+    self.nome = nome
+    return self.nome
+
+def _verificar_email_usuario(self, email):
+
+    self._verificar_email_invalido(email)
+    self._verificar_email_vazio(email)
+
+    self.email = email
+    return self.email
+
+def _verificar_senha_usuario(self, senha):
+
+    self._verificar_senha_invalido(senha)
+    self._verificar_senha_vazio(senha)
+
+    self.senha = senha
+    return self.senha
+
+def _verificar_nome_vazio(self, nome):
+    if not nome.strip():
+        raise ValueError("O que está sendo solicitado está vazio.")
+    return nome
+
+def _verificar_nome_invalido(self, nome):
+    if not isinstance(nome, str):
+        raise TypeError("O que está sendo solicitado está inválido.")
+    return nome
+
+def _verificar_email_vazio(self, email):
+    if not email.strip():
+        raise ValueError("O que está sendo solicitado está vazio.")
+    return email
+
+def _verificar_email_invalido(self, email):
+    if not isinstance(email, str):
+        raise TypeError("O que está sendo solicitado está inválido.")
+    return email
+
+def _verificar_senha_vazio(self, senha):
+    if not senha.strip():
+        raise ValueError("O que está sendo solicitado está vazio.")
+    return senha
+
+def _verificar_senha_invalido(self, senha):
+    if not isinstance(senha, str):
+        raise TypeError("O que está sendo solicitado está inválido.")
+    return senha
 
 # Criando tabela no banco de dados.
 Base.metadata.create_all(bind=db)
